f02469c95757b84a20ec29b1324e35b846a43d99
mike85k87 <mike85k@yandex.ru>
2023-10-05 14:37:08 +0700

    ДЗ 3 начало

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4345;
+        float e = 4.45f;
+        double t = 345.567;
+        System.out.println("Значение переменной a с типом byte равно …"+a);
+        System.out.println("Значение переменной b с типом short равно …"+b);
+        System.out.println("Значение переменной c с типом int равно …"+c);
+    }
+}
